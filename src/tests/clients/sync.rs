d8077fc0fb17b47a2dff7cdea74773dfac75e502
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-02-06 22:18:48 +0300

    [#] Tests (Helpers)

--- src/tests/clients/sync.rs
+++ src/tests/clients/sync.rs
@@ -1,24 +1,20 @@
 #[cfg(test)]
 mod tests {
     use crate::api::requests::sync::Requests;
-    use crate::api::responses::result::ResponseResult;
     use crate::api::types::user::User;
     use crate::tests::helpers::*;
+    use std::fs;
 
     #[test]
     fn get_me() {
-        let mock_response =  "{\"ok\":true,\"result\":{\"id\":6591790550,\"is_bot\":true,\"first_name\":\"development\",\"username\":\"development_bot\",\"can_join_groups\":true,\"can_read_all_group_messages\":false,\"supports_inline_queries\":false}}";
-
+        let mock_response = fs::read_to_string("src/tests/responses/get_me_success.json").unwrap();
         let mut server = mockito::Server::new();
-        let mocked_api = mock_api(&server, mocked_token(None));
-        let mocked_server = mock_server(&mut server, mocked_token(None), "getMe", mock_response);
+        let mocked = Mocked::new(&mut server, "getMe", &mock_response);
 
-        let mock_user = serde_json::from_str::<ResponseResult<User>>(mock_response)
-            .unwrap()
-            .result;
-        let real_user = mocked_api.sync.get_me().unwrap();
+        let mock_user = mocked.result::<User>().unwrap();
+        let real_user = mocked.client.sync.get_me().unwrap();
 
         assert_eq!(mock_user, real_user);
-        mocked_server.assert();
+        mocked.server.assert();
     }
 }
