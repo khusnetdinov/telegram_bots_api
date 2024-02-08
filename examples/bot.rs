b2deba20a94226a836ef7c56b9916e8052a3dab3
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-02-08 11:37:22 +0300

    [#] Specs

--- examples/bot.rs
+++ examples/bot.rs
@@ -6,13 +6,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let api = Client::new();
     let response = api.sync.get_me();
 
-    // let params = SendMessage {
-    //     chat_id: 147951145,
-    //     text: "Hello World".to_string(),
-    //     ..Default::default()
-    // };
-    // let response = api.sync.send_message(&params);
-
     // let params = ForwardMessage {
     //     message_id: 456,
     //     chat_id: 147951145,
