e72e871ee0be8f366f36ec4108da3221fbafc76b
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-02-10 17:41:09 +0300

    [#] Fill files

--- src/api/params/answer_callback_query.rs
+++ src/api/params/answer_callback_query.rs
@@ -4,4 +4,10 @@ use serde::Serialize;
 /// Use this method to send answers to callback queries sent from inline keyboards. The answer will be displayed to the user as a notification at the top of the chat screen or as an alert. On success, True is returned.
 /// Alternatively, the user can be redirected to the specified Game URL. For this option to work, you must first create a game for your bot via @BotFather and accept the terms. Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
 #[derive(Debug, Serialize)]
-pub struct AnswerCallbackQuery {}
+pub struct AnswerCallbackQuery {
+    pub callback_query_id: String,
+    pub text: Option<String>,
+    pub show_alert: Option<bool>,
+    pub url: Option<String>,
+    pub cache_time: Option<i64>
+}
--- src/api/types/inline_query_result_video.rs
+++ src/api/types/inline_query_result_video.rs
@@ -1,7 +1,27 @@
 use serde::{Deserialize, Serialize};
+use crate::api::types::inline_keyboard_markup::InlineKeyboardMarkup;
+use crate::api::types::input_message_content::InputMessageContent;
+use crate::api::types::message_entity::MessageEntity;
 
 /// https://core.telegram.org/bots/api#inlinequeryresultvideo
 /// Represents a link to a page containing an embedded video player or a video file. By default, this video file will be sent by the user with an optional caption. Alternatively, you can use input_message_content to send a message with the specified content instead of the video.
 /// If an InlineQueryResultVideo message contains an embedded video (e.g., YouTube), you must replace its content using input_message_content.
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct InlineQueryResultVideo {}
+pub struct InlineQueryResultVideo {
+    #[serde(rename(serialize = "type", deserialize = "kind"))]
+    pub kind: String,
+    pub id:	String,	
+    pub video_url: String,	
+    pub mime_type: String,	
+    pub thumbnail_url: String,	
+    pub title: String,	
+    pub caption: Option<String>,
+    pub parse_mode: Option<String>,
+    pub caption_entities: Option<Vec<MessageEntity>>,
+    pub video_width: Option<i64>,
+    pub video_height: Option<i64>,
+    pub video_duration: Option<i64>,
+    pub description: Option<String>,
+    pub reply_markup: Option<InlineKeyboardMarkup>,
+    pub input_message_content: Option<InputMessageContent>
+}
