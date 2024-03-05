2ac09559ab57bf22ef2c2ae487eddce18d01e06f
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-03-05 20:35:50 +0300

    [#] Playgrounds

--- examples/sync/playground.rs
+++ examples/sync/playground.rs
@@ -1,8 +1,10 @@
 use std::error::Error;
 use telegram::api::requests::sync::Requests;
 use telegram::clients::sync::Sync;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let response = Sync::new().get_me().unwrap();
+
     println!("{:#?}", response);
 
     Ok(())
