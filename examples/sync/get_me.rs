43f8869d3243e8de5fcd0cb2b29ec4e620ca7ff7
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-03-09 01:50:49 +0300

    # Release 0.0.1 (Naming)

--- examples/sync/get_me.rs
+++ examples/sync/get_me.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use telegram::api::requests::sync::Requests;
-use telegram::clients::sync::Sync;
+use telegram_bots_api::api::requests::sync::Requests;
+use telegram_bots_api::clients::sync::Sync;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let response = Sync::new().get_me().unwrap();
