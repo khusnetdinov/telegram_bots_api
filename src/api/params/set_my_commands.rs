bbd6b9bf02ec6aca24ebca2ec7995c0804bda90c
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-03-17 21:20:48 +0300

    # BotCommands, BotCommandScopes updates

--- src/api/params/set_my_commands.rs
+++ src/api/params/set_my_commands.rs
@@ -1,11 +1,12 @@
 use crate::api::enums::bot_command_scopes::BotCommandScopes;
+use crate::api::structs::bot_command::BotCommand;
 use serde::Serialize;
 
 /// <https://core.telegram.org/bots/api#setmycommands>
 /// Use this method to change the list of the bot's commands. See this manual for more details about bot commands. Returns True on success.
 #[derive(Debug, Serialize, Default)]
 pub struct SetMyCommands {
-    pub commands: Vec<BotCommandScopes>,
+    pub commands: Vec<BotCommand>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub scope: Option<BotCommandScopes>,
     #[serde(skip_serializing_if = "Option::is_none")]
