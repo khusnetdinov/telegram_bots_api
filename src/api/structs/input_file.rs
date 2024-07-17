97705fb25798d7dc490248cfa5e08e85c3c27715
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-07-17 23:38:24 +0300

    # InputFile

--- src/api/structs/input_file.rs
+++ src/api/structs/input_file.rs
@@ -1,6 +1,9 @@
+use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 
 /// <https://core.telegram.org/bots/api#inputfile>
 /// This object represents the contents of a file to be uploaded. Must be posted using multipart/form-data in the usual way that files are uploaded via the browser.
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
-pub struct InputFile {}
+pub struct InputFile {
+    pub path_buf: PathBuf
+}
