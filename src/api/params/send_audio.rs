a0314f043f899fd325337852c3afa997c8d19659
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-07-22 21:44:43 +0300

    # SendAudio

--- src/api/params/send_audio.rs
+++ src/api/params/send_audio.rs
@@ -6,13 +6,15 @@ use crate::api::structs::photo_size::PhotoSize;
 use crate::api::structs::reply_parameters::ReplyParameters;
 use serde::Serialize;
 
-/// <https://core.telegram.org/bots/api#sendanimation>
-/// Use this method to send animation files (GIF or H.264/MPEG-4 AVC video without sound). On success, the sent Message is returned. Bots can currently send animation files of up to 50 MB in size, this limit may be changed in the future.
+/// <https://core.telegram.org/bots/api#sendaudio>
+/// Use this method to send audio files, if you want Telegram clients to display them in the music player. Your audio must be in the .MP3 or .M4A format. On success, the sent Message is returned. Bots can currently send audio files of up to 50 MB in size, this limit may be changed in the future.
+/// For sending voice messages, use the sendVoice method instead.
 #[derive(Debug, Serialize, Default)]
 pub struct SendAudio {
     pub audio: FileInput,
     pub chat_id: ChatUId,
-    pub performer: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub performer: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub message_thread_id: Option<i64>,
     #[serde(skip_serializing_if = "Option::is_none")]
